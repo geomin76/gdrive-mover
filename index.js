77929f29ef14d690cda917724d752e6fa8ced578
geomin76 <geom@vt.edu>
2023-01-03 15:30:50 -0500

    adding express

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const moveAllFiles = require('./service');
 
-moveAllFiles(['DSC01554.ARW', 'DSC01553.ARW'], '1nIK4qNQ4VDIjoY99rNJEdJ0uBIYGlKsw').then(() => {
-  console.log('🔥 All files have been moved!');
-}).catch((error) => {
-    console.error(error)
-});
+// moveAllFiles(['DSC01554.ARW', 'DSC01553.ARW'], '1nIK4qNQ4VDIjoY99rNJEdJ0uBIYGlKsw').then(() => {
+//   console.log('🔥 All files have been moved!');
+// }).catch((error) => {
+//     console.error(error)
+// });
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server don start for port: " + PORT))
