fef5f51cc9f758fdfab03a1a34284a4b0de300f3
Geo Min <31418725+geomin76@users.noreply.github.com>
2023-01-04 11:57:28 -0500

    Web (#1)
    * adding express
    
    * adding views
    
    * added
    
    * set up
    
    * adding form

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const moveAllFiles = require('./service');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-moveAllFiles(['DSC01554.ARW', 'DSC01553.ARW'], '1nIK4qNQ4VDIjoY99rNJEdJ0uBIYGlKsw').then(() => {
-  console.log('🔥 All files have been moved!');
-}).catch((error) => {
-    console.error(error)
+const moveAllFiles = require('./service');
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(session({ secret: "rainbow cat" }));
+
+//middlewares
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.render("form");
 });
+
+app.post("/", function (req, res) {
+    var folder = req.body.folder;
+    req.session.folder = folder;
+    var list = req.body.list;
+    req.session.list = list;
+
+    res.redirect('/result')
+});
+
+app.get('/result', async (req, res) => {
+    if (!req.session.folder || !req.session.list) {
+        res.render('error', {
+            error: "folder or list is empty"
+        })
+    }
+
+    var files = req.session.list.split(/,\s*|\s+/);
+    var folderName = req.session.folder.trim().replace(/\s/g, "")
+
+    await moveAllFiles(files, folderName, res).then(() => {
+        console.log('🔥 All files have been moved!');
+    }).catch((error) => {
+        console.error(error)
+    });
+});
+
+
+app.listen(PORT, console.log("Server started for port: " + PORT))
