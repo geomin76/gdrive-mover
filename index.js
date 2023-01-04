00c8f5d0dd4e13af77ba19c4a81a8cb11167f56b
geomin76 <geom@vt.edu>
2023-01-03 21:40:49 -0500

    set up

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const moveAllFiles = require('./service');
-var bodyParser=require("body-parser");
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(session({ secret: "rainbow cat" }));
 
 //middlewares
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// moveAllFiles(['DSC01552.ARW'], '1nIK4qNQ4VDIjoY99rNJEdJ0uBIYGlKsw').then(() => {
-//   console.log('🔥 All files have been moved!');
-// }).catch((error) => {
-//     console.error(error)
-// });
-
-
-//rendering form.ejs
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("form");
 });
 
-// form submission
-app.get('/result',(req,res)=>{
-    res.render('result');
-});
-  
-//creating form
-app.post("/",function(req,res){
-    var folderId = req.body.folder;
-    console.log(folderId)
+app.post("/", function (req, res) {
+    var folder = req.body.folder;
+    req.session.folder = folder;
     var list = req.body.list;
-    console.log(list)
-    res.redirect("/result");
+    req.session.list = list;
+
+    res.redirect('/result')
 });
+
+app.get('/result', (req, res) => {
+    console.log(req.session.folder)
+    console.log(req.session.list)
+    moveAllFiles(req.session.list, req.session.folder).then(() => {
+      console.log('🔥 All files have been moved!');
+    }).catch((error) => {
+        console.error(error)
+    });
+    res.render('result');
+
+    // try to render results with progress bar, is that possible?
+});
+
 
 app.listen(PORT, console.log("Server started for port: " + PORT))
